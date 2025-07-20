e3757f21231add2e81244a3937f6560554b91250
t8nax <t8naxx@gmail.com>
2025-07-20 16:56:41 +0300

    Fix syntax errors; Add DROP tables

--- database.sql
+++ database.sql
@@ -1,9 +1,16 @@
+DROP TABLE IF EXISTS courses_modules;
+DROP TABLE IF EXISTS modules_programs;
+DROP TABLE IF EXISTS programs;
+DROP TABLE IF EXISTS modules;
+DROP TABLE IF EXISTS lessons;
+DROP TABLE IF EXISTS courses;
+
 CREATE TABLE courses (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    updated_at TIMESTAMP NOT NULL,
     is_deleted BOOLEAN NOT NULL
 );
 
@@ -12,7 +19,7 @@ CREATE TABLE lessons (
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
     video_link VARCHAR(255),
-    position INT NOT NULL
+    position INT NOT NULL,
     created_at TIMESTAMP NOT NULL, 
     updated_at TIMESTAMP NOT NULL,
     course_id BIGINT NOT NULL REFERENCES courses(id),
@@ -29,8 +36,8 @@ CREATE TABLE modules (
 );
 
 CREATE TABLE courses_modules (
-    course_id BIGINT,
-    module_id BIGINT,
+    course_id BIGINT REFERENCES courses(id),
+    module_id BIGINT REFERENCES modules(id),
     PRIMARY KEY(course_id, module_id)
 );
 
@@ -44,7 +51,7 @@ CREATE TABLE programs (
 );
 
 CREATE TABLE modules_programs (
-    module_id BIGINT,
-    program_id BIGINT
+    module_id BIGINT REFERENCES modules(id),
+    program_id BIGINT REFERENCES programs(id),
     PRIMARY KEY(module_id, program_id)
 );
