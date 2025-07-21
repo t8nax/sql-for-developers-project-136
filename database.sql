3d96a733f26fc81b662e0646ae8c125c85a91149
t8nax <t8naxx@gmail.com>
2025-07-21 17:41:11 +0300

    Add tables: enrollments, payments, program_completions, certificates

--- database.sql
+++ database.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS certificates;
+DROP TABLE IF EXISTS program_completions;
+DROP TABLE IF EXISTS payments;
+DROP TABLE IF EXISTS enrollments;
 DROP TABLE IF EXISTS courses_modules;
 DROP TABLE IF EXISTS modules_programs;
 DROP TABLE IF EXISTS programs;
@@ -6,6 +10,11 @@ DROP TABLE IF EXISTS lessons;
 DROP TABLE IF EXISTS courses;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS teaching_groups;
+
+DROP TYPE IF EXISTS user_role;
+DROP TYPE IF EXISTS enrollment_status;
+DROP TYPE IF EXISTS payment_status;
+DROP TYPE IF EXISTS program_completion_status;
 
 CREATE TABLE courses (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -65,13 +74,60 @@ CREATE TABLE teaching_groups (
     updated_at TIMESTAMP NOT NULL
 );
 
+CREATE TYPE user_role AS ENUM ('student', 'teacher', 'admin');
+
 CREATE TABLE users (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     pass_hash VARCHAR(255) NOT NULL,
     teaching_group_id BIGINT REFERENCES teaching_groups(id),
-    role VARCHAR(255) NOT NULL,
+    role user_role NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TYPE enrollment_status AS ENUM ('active', 'pending', 'cancelled', 'completed');
+
+CREATE TABLE enrollments (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT NOT NULL REFERENCES users(id),
+    program_id BIGINT NOT NULL REFERENCES programs(id),
+    status enrollment_status NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TYPE payment_status AS ENUM ('pending', 'paid', 'failed', 'refunded');
+
+CREATE TABLE payments (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    enrollment_id BIGINT NOT NULL REFERENCES enrollments(id),
+    status payment_status NOT NULL,
+    paid_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TYPE program_completion_status AS ENUM ('active', 'completed', 'pending', 'cancelled');
+
+CREATE TABLE program_completions (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT NOT NULL REFERENCES users(id),
+    program_id BIGINT NOT NULL REFERENCES programs(id),
+    status program_completion_status NOT NULL,
+    started_at TIMESTAMP NOT NULL,
+    ended_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE certificates (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT NOT NULL REFERENCES users(id),
+    program_id BIGINT NOT NULL REFERENCES programs(id),
+    url VARCHAR(1023) NOT NULL,
+    issued_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
